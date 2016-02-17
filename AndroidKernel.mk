128c8f9e1632a28301c07d1fd4883f282231046d
vasishath <vasishath@gmail.com>
2016-02-17 14:26:57 +0000

    Update tc path
    Change-Id: I127dfdcfcf8dd9f860d1390515cd223a6bca7160

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -19,7 +19,7 @@ KERNEL_HEADERS_INSTALL := $(KERNEL_OUT)/usr
 KERNEL_MODULES_INSTALL := system
 KERNEL_MODULES_OUT := $(TARGET_OUT)/lib/modules
 KERNEL_IMG=$(KERNEL_OUT)/arch/arm/boot/Image
-CROSS_COMPILE_PATH=/home/vasishath/toolchain/arm-eabi-5.3/bin/
+CROSS_COMPILE_PATH=/home/vasishath/roms/toolchain/arm-eabi-5.3/bin/
 
 DTS_NAMES ?= $(shell $(PERL) -e 'while (<>) {$$a = $$1 if /CONFIG_ARCH_((?:MSM|QSD|MPQ)[a-zA-Z0-9]+)=y/; $$r = $$1 if /CONFIG_MSM_SOC_REV_(?!NONE)(\w+)=y/; $$arch = $$arch.lc("$$a$$r ") if /CONFIG_ARCH_((?:MSM|QSD|MPQ)[a-zA-Z0-9]+)=y/} print $$arch;' $(KERNEL_CONFIG))
 KERNEL_USE_OF ?= $(shell $(PERL) -e '$$of = "n"; while (<>) { if (/CONFIG_USE_OF=y/) { $$of = "y"; break; } } print $$of;' kernel/arch/arm/configs/$(KERNEL_DEFCONFIG))
