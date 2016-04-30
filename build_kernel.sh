0614ecbb114ec7d004ddda07959c7082c5fc6a9b
Vasishath Kaushal <vasishath@gmail.com>
2016-04-30 07:44:00 +0000

    Change gcc version
    Change-Id: Ida7bbe671d2ec6329dc750c34184637743f59f95

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,6 @@
-CROOT=~/xenon
 DEFCONFIG=cyanogen_cancro_defconfig
-make ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-5.3/bin/arm-eabi- $DEFCONFIG
-make -j32 ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-5.3/bin/arm-eabi- | tee xmlog 2>&1
+make ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-7.0/bin/arm-eabi- $DEFCONFIG
+make -j32 ARCH=arm CROSS_COMPILE=~/toolchain/arm-eabi-7.0/bin/arm-eabi-
 for i in `find -name *.ko`; do cp $i ~/anykernel/AnyKernel2/modules/; done
 dtbToolCM -s 2048 -d "qcom,msm-id = <" -2 -o arch/arm/boot/dt.img -p /usr/bin/ arch/arm/boot/
 cp arch/arm/boot/zImage ~/anykernel/AnyKernel2/
@@ -10,5 +9,4 @@ cd ~/anykernel/AnyKernel2
 DATE=$(date +"%m-%d-%y")
 rm Z*
 zip -r9 ZeurionX-$DATE.zip * -x README ZeurionX-$DATE.zip
-sudo cp Z* /var/www/html/caf/
-sudo chown -R www-data:www-data /var/www/html/caf/
+sudo cp Z* ~/public_html/
